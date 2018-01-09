57b6ebbc1609e799c0c7da1ae73688473fe8a73e
Sara Cope <sara.cope@gsa.gov>
2018-01-09 14:47:14 -0500

    Add down arrow

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,5 +33,9 @@ $('.value').each(function() {
 	$(this).parent().css('width', text);
 });
 
-// $('.block').tooltip();
+$('.block').tooltip();
 
+$(window).scroll(function(){
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+  //250 is fade pixels
+  });
